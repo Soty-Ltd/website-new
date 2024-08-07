c962c8ec2daa2a4a2770097cf0fbd7a3d1abfbde
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-08-07 23:31:56 +0200

    website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>Learn More</nav>
+<section class="hero">
+	<h1>We turn your on-premises applications <br /> into a dynamic cloud solution in seconds.</h1>
+	<button>Learn More</button>
+</section>
+<section class="us">
+	<h2>
+		We are a Company from the United Kingdom <br /> that provides you with the perfect Servers
+		<br /> to get you started for your Project
+	</h2>
+	<img src="northshields.jpg" alt="" />
+</section>
