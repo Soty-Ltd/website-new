4989eed37a0c5479faf15fa614a7b17ff3545b48
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-08-09 21:20:08 +0200

    new sections

---
+++ src/routes/+error.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<section class="error">
+	<img src="sadhamster.png" alt="" width="150px" />
+
+	<div class="seperator"></div>
+
+	<div style="display: flex; flex-direction:column; align-items:start; margin:25px;">
+		<h1>{$page.status} {$page.error.message}</h1>
+		<h3>
+			{#if (page.status = 404)}
+				We couldn't find the page you are looking for, sowwy ):
+				<br />
+				<button>Go Back Home</button>
+			{/if}
+		</h3>
+	</div>
+</section>
+
+<style>
+	.error {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.seperator {
+		border: 2px solid black;
+		height: 30%;
+		margin: 0 0 0 25px;
+	}
+	button {
+		margin: 10px 0 0 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,9 +33,29 @@
 		const y = Math.random() * 90; // Random y position (0-90%)
 		return { x, y };
 	}
+
+	function getServerPing() {
+		const ping = Math.random() * 30;
+		const ping_round = Math.round(ping);
+		return ping_round;
+	}
+
+	let dataTB = 514518400;
+
+	function addNumber() {
+		dataTB = dataTB + 10;
+		console.log('hey');
+	}
+
+	const interval = setInterval(addNumber, 10);
 </script>
 
-<nav>Learn More</nav>
+<nav>
+	<img src="logo.png" alt="" width="10%" />
+	<ul>
+		<li><a href="#">hello</a></li>
+	</ul>
+</nav>
 <section class="hero">
 	<h1>We turn your on-premises applications <br /> into a dynamic cloud solution in seconds.</h1>
 	<button>Learn More</button>
@@ -60,4 +80,40 @@
 			{paragraph}
 		</div>
 	{/each}
+</section>
+<section class="datacenters">
+	<h1>All of our Datacenters and <br /> probaly one for you</h1>
+	<div class="locations">
+		<div>London, UK <br /> {getServerPing()}ms</div>
+		<div>Frankfurt, Germany <br /> {getServerPing()}ms</div>
+		<div>New York, USA <br /> {getServerPing()}ms</div>
+		<div>Stockholm, Sweden <br /> {getServerPing()}ms</div>
+		<div>Tokyo, Japan <br /> {getServerPing()}ms</div>
+		<div>Toronto, Canada <br /> {getServerPing()}ms</div>
+	</div>
+</section>
+<section class="prices">
+	<h2>Some of our Products</h2>
+	<div class="products">
+		<div class="product">
+			<h2>VPS</h2>
+			<h3>Starting at 4.59$</h3>
+			<button>-></button>
+		</div>
+		<div class="product">
+			<h2>VPS</h2>
+			<h3>Starting at 4.59$</h3>
+			<button>-></button>
+		</div>
+		<div class="product">
+			<h2>VPS</h2>
+			<h3>Starting at 4.59$</h3>
+			<button>-></button>
+		</div>
+	</div>
+</section>
+<section class="data_delivered">
+	<h2>We delivered over</h2>
+	<h1>{dataTB.toLocaleString()} Gigabyte</h1>
+	<h2>of Data</h2>
 </section>
