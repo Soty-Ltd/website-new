27ab18d0a11f6794296902f91c0fe1fe707e0eff
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-08-13 19:20:10 +0200

    improved mobile nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -84,8 +84,8 @@
 	}
 	let nav = 'none';
 	function mobileNav() {
-		if ((nav = 'none')) {
-			nav = 'block';
+		if (nav == 'none') {
+			nav = 'flex';
 		} else {
 			nav = 'none';
 		}
@@ -116,7 +116,21 @@
 		<div class="signup_a"><li><a href="/sign-up">Sign Up</a></li></div>
 	</ul>
 </nav>
+<!-- Navigation Bar for Mobile Dvices -->
 <div class="mobile_nav" style="display: {nav};">
+	<div on:click={() => mobileNav()}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			height="40px"
+			viewBox="0 -960 960 960"
+			width="40px"
+			fill="#000"
+			><path
+				d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+			/></svg
+		>
+	</div>
+
 	<ul>
 		<li><a href="/">Home</a></li>
 		<li><a href="/news">News</a></li>
