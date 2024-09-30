a08e54995283589a7b34fb70acff3cc77c761755
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-09-30 23:07:27 +0200

    refactored contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,78 +10,53 @@
 	image={'s'}
 />
 
-<main class="px-20 h-[150vh] flex items-center justify-center flex-col">
-	<section class="contact flex justify-between items-center">
-		<div class="contactUs mr-5">
-			<h1 class="text-left text-5xl">Contact Us - We're Here to Help</h1>
-			<h2 class="text-4xl">Get in Touch with Our Team</h2>
-			<p class="text-xl">
+<main class="flex flex-col items-center justify-center w-screen h-max p-8 py-20 max-sm:h-max">
+	<div class="flex items-center justify-between max-sm:flex-col">
+		<div class="w-1/2 m-3 max-sm:w-full">
+			<h1 class="text-5xl text-left">Contact Us - We're Here to Help</h1>
+			<h2 class="text-2xl">Get in Touch with Our Team</h2>
+			<p>
 				We value your feedback, questions, and any concerns you may have. Our dedicated support team
 				is ready to assist you with whatever you need. Whether you're looking for product
 				information, need help with an order, or just want to share your thoughts, we’re just a
 				message away.
 			</p>
 		</div>
-		<img class="office rounded-3xl" src="/office.jpg" alt="Soty Ltd. Office" width="600px" />
-	</section>
-	<section class="contactInfo flex items-center justify-between m-10 w-screen">
-		<div class="contactWay">
-			<img src="/icons/mail.svg" alt="" />
-			<h3>Write us an E-Mail</h3>
-			<a href="mailto:info@soty.one">info@soty.one</a>
+		<div class="w-[600px] m-3 max-sm:w-full">
+			<img src="/office.jpg" class="rounded-2xl" alt="Office from Soty Ltd." />
 		</div>
-		<div class="contactWay m-3">
-			<img src="/icons/mail.svg" alt="" />
-			<h3>Call Us</h3>
-			<a href="tel:">+1</a>
+	</div>
+
+	<div class="flex justify-between w-full mt-10 max-sm:flex-col max-sm:m-0">
+		<div class="bg-[#fafceb] rounded-2xl p-7 m-3 w-1/3 max-sm:mx-0 max-sm:w-full">
+			<span class="flex items-center">
+				<img src="/icons/mail.svg" class="mr-2" alt="" />
+				<h3>Mail</h3>
+			</span>
+			<span>
+				<a href="mailto:info@soty.one">info@soty.one</a>
+			</span>
 		</div>
-		<div class="contactWay">
-			<img src="/icons/mail-adr.svg" alt="" />
-			<h3>Send us a Letter</h3>
-			<address itemscope itemtype="https://schema.org/PostalAddress">
-				<span itemprop="streetAddress">Unit 6, Jupiter Court</span><br />
-				<span itemprop="addressLocality">Orion Business Park</span><br />
-				<span itemprop="addressRegion">North Shields</span>,
-				<span itemprop="postalCode">NE29 7SN</span><br />
-				<span itemprop="addressCountry">United Kingdom</span>
-			</address>
+		<div class="bg-[#fafceb] rounded-2xl p-7 m-3 w-1/3 max-sm:mx-0 max-sm:w-full">
+			<span class="flex items-center">
+				<img src="/icons/call.svg" class="mr-2" alt="" />
+				<h3>Phone</h3>
+			</span>
+			<span>
+				<a href="tel:+19176249703">+1 917-624-9703</a>
+			</span>
 		</div>
-	</section>
+		<div class="bg-[#fafceb] rounded-2xl p-7 m-3 w-1/3 max-sm:mx-0 max-sm:w-full">
+			<span class="flex items-center">
+				<img src="/icons/mail-adr.svg" class="mr-2" alt="" />
+				<h3>Address</h3>
+			</span>
+			<span>
+				<address>
+					Unit 6, Jupiter Court <br /> Orion Business Park North Shields,<br /> NE29 7SN <br /> United
+					Kingdom
+				</address>
+			</span>
+		</div>
+	</div>
 </main>
-
-<style>
-	@media screen and (max-width: 600px) {
-		main {
-			padding: 0 30px;
-			height: 240vh;
-		}
-		.contact {
-			flex-direction: column;
-			padding: 0 0 0 0;
-			justify-content: center;
-		}
-		.office {
-			width: 90vw;
-			margin: 20px 0 0 0;
-		}
-		.contactInfo {
-			display: block;
-			width: 100%;
-		}
-
-		#middle {
-			margin: 0;
-		}
-
-		.contactWay {
-			margin: 20px 0;
-		}
-	}
-	.contactWay {
-		background-color: #f9fce9;
-		padding: 20px;
-		border-radius: 20px;
-		width: 90%;
-		height: 130px;
-	}
-</style>
