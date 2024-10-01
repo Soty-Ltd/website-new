01a2f6d70d407dcbf39a783a6cd308c4be6b446a
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-10-01 17:51:09 +0200

    news page tailwind

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -10,142 +10,36 @@
 	description={'Soty Ltd. - Discover the talented team behind Soty, leaders in cloud computing. Meet our executives, engineers, and support staff who drive innovation and excellence. Learn more about our skilled professionals and their roles.'}
 	image={'s'}
 />
-<div class="heroNews">
-	<div style="margin: 30px; ">
-		<h2>Newsroom</h2>
-		<hr />
-		<br />
-		<h1>Soty Ltd. Enhances Frankfurt <br /> Datacenter with 15 Gigabit Connection</h1>
-		<p>Boosting Performance and Reliability for Enhanced Digital Services</p>
-		<button on:click={() => location.replace('/news/article/?article=0')}>Read More</button>
+<main class="">
+	<div
+		class="heroNews w-screen h-max bg-[#f9fce9] flex justify-between items-center p-9 pt-20 pb-20 max-sm:flex-col"
+	>
+		<div class="">
+			<h2>Newsroom</h2>
+			<hr />
+			<h1 class="text-left">
+				Soty Ltd. Enhances Frankfurt <br /> Datacenter with 15 Gigabit Connection
+			</h1>
+			<p class="mt-2">Boosting Performance and Reliability for Enhanced Digital Services</p>
+			<button class="mt-3" on:click={() => location.replace('/news/article/?article=0')}
+				>Read More</button
+			>
+		</div>
+
+		<img
+			src="/news/servers.webp"
+			class="h-[70vh] w-[50vw] max-sm:w-full max-sm:h-full mt-8"
+			alt=""
+		/>
 	</div>
-
-	<div class="headingImg"></div>
-</div>
-
-<main>
-	<div class="grid">
-		<div class="news">
-			<img src="news/hello.webp" alt="" /><br />
+	<div class="grid p-8">
+		<div class="news w-1/3 bg-[#f9fce9] p-8 rounded-xl max-sm:w-full">
+			<img src="news/hello.webp" class="w-full" alt="" /><br />
 			<h2>Soty Ltd. Enhances Frankfurt <br /> Datacenter with 15 Gigabit Connection</h2>
 			<p>Boosting Performance and Reliability for Enhanced Digital Services</p>
-			<button on:click={() => location.replace('/news/article/?article=1')}>Read More</button>
-		</div>
-		<div class="news">
-			<img src="news/hello.webp" alt="" /><br />
-			<h2>Soty Ltd. Enhances Frankfurt <br /> Datacenter with 15 Gigabit Connection</h2>
-			<p>Boosting Performance and Reliability for Enhanced Digital Services</p>
-			<button on:click={() => location.replace('/news/article/?article=2')}>Read More</button>
-		</div>
-		<div class="news">
-			<img src="news/hello.webp" alt="" /><br />
-			<h2>Soty Ltd. Enhances Frankfurt <br /> Datacenter with 15 Gigabit Connection</h2>
-			<p>Boosting Performance and Reliability for Enhanced Digital Services</p>
-			<button on:click={() => location.replace('/news/article/?article=3')}>Read More</button>
-		</div>
-		<div class="news">
-			<img src="news/hello.webp" alt="" /><br />
-			<h2>Soty Ltd. Enhances Frankfurt <br /> Datacenter with 15 Gigabit Connection</h2>
-			<p>Boosting Performance and Reliability for Enhanced Digital Services</p>
-			<button on:click={() => location.replace('/news/article/?article=4')}>Read More</button>
+			<button class="mt-3" on:click={() => location.replace('/news/article/?article=1')}
+				>Read More</button
+			>
 		</div>
 	</div>
 </main>
-
-<style>
-	* {
-		margin: 0;
-		padding: 0;
-	}
-	.heroNews {
-		width: 100vw;
-		height: max-content;
-		background-color: #f9fce9;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 30px 0 0 0;
-	}
-
-	.headingImg {
-		background: url('/news/servers.webp') center;
-		background-size: cover;
-		height: 80vh;
-		width: 50vw;
-	}
-	h1 {
-		text-align: left;
-	}
-
-	h2 {
-		font-weight: bolder;
-	}
-
-	p {
-		font-size: 1.25rem;
-	}
-
-	button {
-		border: 1px solid black;
-		background: none;
-		color: black;
-		padding: 10px;
-		margin: 10px 0 0 0;
-		border-radius: 100px;
-	}
-	button:hover {
-		background-color: #000;
-		color: #fff;
-	}
-
-	main {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		grid-template-rows: repeat(2, 1fr);
-	}
-
-	.news {
-		background-color: #f9fce9;
-		width: 40vw;
-		padding: 40px;
-		margin: 20px;
-		box-sizing: border-box;
-	}
-
-	.news img {
-		max-width: 100%;
-	}
-
-	.news h2 {
-		font-size: 2rem;
-	}
-
-	@media screen and (max-width: 600px) {
-		.heroNews {
-			display: block;
-			margin: 0px;
-		}
-
-		.headingImg {
-			width: 100vw;
-			height: 60vw;
-		}
-
-		.grid {
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			flex-direction: column;
-		}
-
-		.news {
-			width: 80vw;
-			margin: 20px;
-		}
-	}
-</style>
